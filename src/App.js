5ecd52fdcef4ccfc3b780f93b7bc0cb3b2624836
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-11-16 20:36:03 +0700

    feat: add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import ProductPage from "./components/ProductPage";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Header/>
-        <Main />
-      </div>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="products" element={<ProductPage />} />
+        </Routes>
+      </>
     );
   }
 }
