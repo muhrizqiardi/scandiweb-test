664a4a7c324cc64cc18dcbf49d90940290fdc83a
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-11-16 19:04:05 +0700

    fix: global style

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: white;
+    margin: 0;
     font-family: Raleway, Arial, Helvetica, sans-serif;
   }
 `;
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyle>
-      <App />
-    </GlobalStyle>
+    <GlobalStyle />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
