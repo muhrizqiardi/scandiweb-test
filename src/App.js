d2d16c7c000f1d5b5ed01aa88122b410c7a0261d
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-11-17 10:32:20 +0700

    feat: add cart page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import CartPage from "./components/CartPage";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import ProductPage from "./components/ProductPage";
@@ -12,6 +13,7 @@ export default class App extends Component {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="products" element={<ProductPage />} />
+          <Route path="cart" element={<CartPage />} />
         </Routes>
       </>
     );
