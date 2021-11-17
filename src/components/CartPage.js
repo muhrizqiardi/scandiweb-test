191747cb9c8ceab8d2e832335fdeb3c2668a3cea
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-11-17 10:42:24 +0700

    feat: cart page wip

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -11,7 +11,21 @@ const Wrapper = styled.main`
   & .cart-list {
     & .cart-item {
       height: 225px;
+      padding: 16px;
       border-top: 1px solid #e5e5e5;
+      & .item-brand {
+        margin-bottom: 16px;
+        font-size: 32px;
+        font-weight: 700;
+      }
+      & .item-name {
+        margin-bottom: 20px;
+        font-size: 32px;
+      }
+      & .item-price {
+        font-size: 24px;
+        font-weight: bold;
+      }
     }
   }
 `;
@@ -23,7 +37,9 @@ export default class CartPage extends Component {
         <h1>Cart</h1>
         <div className="cart-list">
           <div className="cart-item">
-            
+            <div class="item-brand">Apollo</div>
+            <div class="item-name">Running Short</div>
+            <div class="item-price">$50.00</div>
           </div>
         </div>
       </Wrapper>
