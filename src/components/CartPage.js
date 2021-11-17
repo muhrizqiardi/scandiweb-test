627a2dc79ec1553b25d17d6d3b0c84d6f752b00b
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-11-18 06:03:08 +0700

    feat: cart page completed

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -10,21 +10,83 @@ const Wrapper = styled.main`
   }
   & .cart-list {
     & .cart-item {
-      height: 225px;
+      height: 200px;
       padding: 16px;
       border-top: 1px solid #e5e5e5;
-      & .item-brand {
-        margin-bottom: 16px;
-        font-size: 32px;
-        font-weight: 700;
+      display: grid;
+      grid-template-columns: 1fr 45px 150px;
+      gap: 20px;
+      & .cart-item-col-1 {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        & .item-brand {
+          font-size: 32px;
+          font-weight: 700;
+        }
+        & .item-name {
+          font-size: 32px;
+        }
+        & .item-price {
+          font-size: 24px;
+          font-weight: bold;
+        }
+        & .size-selector {
+          margin-top: 10px;
+          display: flex;
+          flex-direction: row;
+          & .size-item {
+            width: 40px;
+            height: 40px;
+            margin-right: 10px;
+            border: 1px solid black;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            cursor: pointer;
+            &:hover {
+              background-color: gray;
+            }
+            &.selected {
+              background-color: black;
+              color: white;
+            }
+            &.not-available {
+              filter: opacity(0.4);
+              &:hover {
+                background-color: white;
+                cursor: not-allowed;
+              }
+            }
+          }
+        }
       }
-      & .item-name {
-        margin-bottom: 20px;
-        font-size: 32px;
+      & .cart-item-col-2 {
+        & .qty-counter {
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-items: center;
+          & button {
+            border: none;
+            background: none;
+            padding: 0;
+
+            width: 45px;
+            height: 45px;
+            border: 1px solid black;
+            /* font-size: 1.3em; */
+          }
+        }
       }
-      & .item-price {
-        font-size: 24px;
-        font-weight: bold;
+      & .cart-item-col-3 {
+        width: 150px;
+        height: 200px;
+        & img {
+          height: 200px;
+          width: 150px;
+        }
       }
     }
   }
@@ -37,9 +99,32 @@ export default class CartPage extends Component {
         <h1>Cart</h1>
         <div className="cart-list">
           <div className="cart-item">
-            <div class="item-brand">Apollo</div>
-            <div class="item-name">Running Short</div>
-            <div class="item-price">$50.00</div>
+            <div class="cart-item-col-1">
+              <div class="item-brand">Apollo</div>
+              <div class="item-name">Running Short</div>
+              <div class="item-price">$50.00</div>
+              <div class="size-selector">
+                <div class="size-item">XS</div>
+                <div class="size-item">S</div>
+                <div class="size-item">M</div>
+                <div class="size-item">L</div>
+              </div>
+            </div>
+            <div class="cart-item-col-2">
+              <div class="qty-counter">
+                <button>+</button>
+                <span>15</span>
+                <button>-</button>
+              </div>
+            </div>
+            <div class="cart-item-col-3">
+              <div class="cart-gallery">
+                <img
+                  src="http://unsplash.it/150/200?random&gravity=center"
+                  alt=""
+                />
+              </div>
+            </div>
           </div>
         </div>
       </Wrapper>
