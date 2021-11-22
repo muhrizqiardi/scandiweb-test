f9767a4e465c6480e59c88c1e030ebc5b4c97ae0
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-11-22 07:50:19 +0700

    feat: create cartcontext

---
+++ src/context/CartContext.js
@@ -0,0 +1,83 @@
+import React from "react";
+
+const CartContext = React.createContext();
+
+export const CartConsumer = CartContext.Consumer;
+
+export class CartProvider extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cart: [],
+    };
+
+    this.addItemToCart = this.addItemToCart.bind(this);
+    this.decreaseItemFromCart = this.decreaseItemFromCart.bind(this);
+  }
+
+  addItemToCart(newCartItem) {
+    this.setState(
+      (state) => {
+        let sameCartItem = state.cart.filter(
+          (cartItem) => cartItem.productId === newCartItem.productId
+        )[0];
+        if (sameCartItem) {
+          let newCart = [...state.cart];
+          let indexOfSameCartItem = newCart.indexOf(sameCartItem);
+          newCartItem.quantity = newCart[indexOfSameCartItem].quantity + 1;
+          newCart[indexOfSameCartItem] = newCartItem;
+          return {
+            cart: newCart,
+          };
+        } else {
+          return {
+            cart: [...state.cart, newCartItem],
+          };
+        }
+      },
+      () => {
+        console.log("cart:", this.state.cart);
+      }
+    );
+    console.log(this.state.cart);
+  }
+
+  decreaseItemFromCart(productId) {
+    this.setState((state) => {
+      let productInCart = [...state.cart].filter(
+        (cartItem) => cartItem.productId === productId
+      )[0];
+      console.log("productInCart 1", productInCart.quantity);
+
+      if (productInCart) {
+        let newCart = [...state.cart];
+        let indexOfItem = newCart.indexOf(productInCart);
+        if (newCart[indexOfItem].quantity === 1) {
+          newCart = newCart.filter((item) => item !== newCart[indexOfItem]);
+          return {
+            ...state,
+            cart: newCart,
+          };
+        } else {
+          newCart[indexOfItem].quantity -= 1;
+          console.log(newCart[indexOfItem].quantity);
+          return {
+            ...state,
+            cart: newCart,
+          };
+        }
+      }
+    });
+  }
+
+  render() {
+    return (
+      <CartContext.Consumer value={this.state.cart, this.addItemToCart, this.decreaseItemFromCart}>
+        {this.props.children}
+      </CartContext.Consumer>
+    )
+  }
+}
+
+export default CartContext;
