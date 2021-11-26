9567128aa01498d695b6fb3d7ab07541f56345c9
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-11-27 05:36:11 +0700

    feat: changed 404 page width

--- src/components/NoMatch404.js
+++ src/components/NoMatch404.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 80vh;
   display: flex;
   flex-direction: column;
