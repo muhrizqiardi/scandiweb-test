983c70d229f9e179b13db60703162e1d41bdafb3
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-11-28 05:28:18 +0700

    ref: removed unnecessary context

--- src/components/CartPage/CartPage.js
+++ src/components/CartPage/CartPage.js
@@ -1,7 +1,6 @@
 import CartItem from "./CartItem";
 import { CartPageTitle } from "./styles";
 import React, { Component } from "react";
-import { CartConsumer } from "../../contexts/CartContext";
 import { CartPageWrapper } from "./styles";
 import { connect } from "react-redux";
 import { incrementItem, decrementItem } from "../../store/actions";
@@ -9,28 +8,19 @@ import { incrementItem, decrementItem } from "../../store/actions";
 class CartPage extends Component {
   render() {
     return (
-      <CartConsumer>
-        {(context) => (
-          <CartPageWrapper>
-            <CartPageTitle>Cart</CartPageTitle>
-            <div className="cart-list">
-              {this.props.cart.map((cartItem) => (
-                <CartItem
-                  apolloClient={this.props.apolloClient}
-                  currency={this.props.currency}
-                  cartItem={cartItem}
-                  productId={cartItem.productId}
-                  quantity={cartItem.quantity}
-                  attributes={cartItem.attributes}
-                  addItemToCart={context.addItemToCart}
-                  decreaseItemFromCart={context.decreaseItemFromCart}
-                  key={cartItem.id}
-                />
-              ))}
-            </div>
-          </CartPageWrapper>
-        )}
-      </CartConsumer>
+      <CartPageWrapper>
+        <CartPageTitle>Cart</CartPageTitle>
+        <div className="cart-list">
+          {this.props.cart.map((cartItem) => (
+            <CartItem
+              apolloClient={this.props.apolloClient}
+              currency={this.props.currency}
+              cartItem={cartItem}
+              key={cartItem.id}
+            />
+          ))}
+        </div>
+      </CartPageWrapper>
     );
   }
 }
